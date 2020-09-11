c1730804c6e79e3ae71e667e0c206c8ca1ce1cbb
lixiangyun <linimbus@126.com>
2020-09-11 09:49:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 mkdir -p /home/data/etcd
 mkdir -p /home/log/etcd
 
-nohup etcd --data-dir /home/data/etcd 2>&1 1>/home/log/etcd/etcd.log etcd --listen-client-urls http://localhost:18003 --listen-peer-urls http://localhost:18002 --advertise-client-urls http://localhost:18003 &
+nohup etcd --data-dir /home/data/etcd --listen-client-urls http://localhost:18003 --listen-peer-urls http://localhost:18002 --advertise-client-urls http://localhost:18003 >/home/log/etcd/etcd.log &
 sleep 5
 
 mkdir -p /home/data/console
